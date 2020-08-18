fc33a892ca6fb3f5ccc3ee9c5a3177ccc6351c09
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-08-18 00:22:18 -0300

    Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hero = () => (
+  <div>
+    <div>
+      <h1>LRM Colheitas</h1>
+      <ul>
+        <li>Lorem Ipsum</li>
+        <li>Oiado lorem as</li>
+        <li>Niasoa ein asles</li>
+      </ul>
+      <button>Entre em contato</button>
+    </div>
+  </div>
+);
+
+export default Hero;
