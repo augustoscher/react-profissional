b1fc691cc2f15abab7c6dd5ffe15a8faa165a11b
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-08-19 15:00:39 -0300

    testing with RTL and Jest

---
+++ src/components/Hero.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Hero from './Hero';
+
+// best practice is look at component proptypes
+// to know if we're testing all component behavior.
+
+test('renders Hero with children', () => {
+  const { getByText } = render(
+    <Hero>
+      <p>Oi</p>
+    </Hero>
+  );
+
+  expect(getByText('Oi')).toBeInTheDocument();
+});
+
+test('renders background image', () => {
+  const image = 'http://test/test.jpg';
+
+  const { getByTestId } = render(<Hero image={image} />);
+
+  //jest-styled-components package
+  expect(getByTestId('hero')).toHaveStyleRule({
+    backgroundImage: image,
+  });
+});
