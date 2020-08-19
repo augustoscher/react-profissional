2915b1de42d67d193e7467c34b41a0cfeda89972
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-08-18 22:21:24 -0300

    Heading stories

---
+++ src/stories/Heading.stories.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Heading from '../components/Heading';
+
+export default {
+  title: 'Heading',
+  component: Heading,
+};
+
+export const usage = () => (
+  <>
+    <Heading>
+      <h1>Title 1</h1>
+    </Heading>
+    <Heading>
+      <h2>Title 2</h2>
+    </Heading>
+    <Heading>
+      <h3>Title 3</h3>
+    </Heading>
+    <Heading>
+      <h4>Title 4</h4>
+    </Heading>
+    <Heading>
+      <h5>Title 5</h5>
+    </Heading>
+    <Heading>
+      <h6>Title 6</h6>
+    </Heading>
+  </>
+);
