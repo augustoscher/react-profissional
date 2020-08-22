dd71d9c4b99a717c1930e5f340c32f91911cf4b3
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-08-21 23:50:31 -0300

    testing button click

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '../test-utils';
+import { render, fireEvent } from '../test-utils';
 import Button from './Button';
 
 test('renders text', () => {
   const { getByText } = render(<Button>Click here</Button>);
 
   expect(getByText('Click here')).toBeInTheDocument();
+});
+
+test('triggers event on click', () => {
+  const handleClick = jest.fn();
+
+  const { getByRole } = render(<Button onClick={handleClick} />);
+  fireEvent.click(getByRole('button'));
+
+  expect(handleClick).toBeCalled();
 });
