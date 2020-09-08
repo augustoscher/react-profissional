4c39f20d94fd51db465797879989f9b08022fc86
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-09-08 02:14:52 -0300

    Adding Feature storie

--- src/components/atoms/Feature.js
+++ src/components/atoms/Feature.js
@@ -1,12 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+const Root = styled.div`
+  text-align: center;
+
+  & h5 {
+    margin: 16px 0;
+  }
+
+  & p {
+    margin: 0;
+  }
+`;
+
+const getPrimaryColor = props => props.theme.colors.primary.main;
+
+const RootIcon = styled.div`
+  display: inline-block;
+  width: 116px;
+  height: 116px;
+  padding: 8px;
+  border-radius: 50%;
+
+  &:hover {
+    border: 1px solid ${getPrimaryColor};
+
+    /* pega qqer filho abaixo do elemento RootIcon */
+    > * {
+      color: #fff;
+      background-color: ${getPrimaryColor};
+    }
+  }
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #e9e9e9;
+  border-radius: 50%;
+  font-size: 32px;
+  width: 100%;
+  height: 100%;
+  color: ${getPrimaryColor};
+`;
+
 const Feature = ({ icon, title, children }) => (
-  <div>
-    <div>{icon}</div>
+  <Root>
+    <RootIcon>
+      <IconContainer>{icon}</IconContainer>
+    </RootIcon>
     <h5>{title}</h5>
     <div>{children}</div>
-  </div>
+  </Root>
 );
 
 Feature.defaultProps = {
--- src/components/atoms/Feature.stories.js
+++ src/components/atoms/Feature.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaCar } from 'react-icons/fa';
+import styled from 'styled-components';
+import { FaCar, FaKey, FaProjectDiagram, FaMapMarkerAlt } from 'react-icons/fa';
 import Feature from './Feature';
 
 export default {
@@ -8,7 +9,40 @@ export default {
 };
 
 export const usage = () => (
-  <Feature icon={<FaCar />} title="Teste">
-    Mais testes
+  <Feature icon={<FaCar />} title="Título">
+    <p>Textinho de teste</p>
   </Feature>
+);
+
+const Grid = styled.div`
+  display: flex;
+`;
+
+export const withGrid = () => (
+  <Grid>
+    <Feature icon={<FaKey />} title="Variada frota de colheitadeiras">
+      <p>
+        Temos variada frota de colheitadeiras. Nos ajustamos de acordo com sua
+        necessidade de colheita.
+      </p>
+    </Feature>
+    <Feature icon={<FaCar />} title="Transporte de implementos agrícolas">
+      <p>
+        Transportamos com agilidade e segurança seus implementos agrícolas,
+        escavadeiras, caminhões entre outros.
+      </p>
+    </Feature>
+    <Feature icon={<FaMapMarkerAlt />} title="Plantamos sua lavoura">
+      <p>
+        Contamos com todos os implementos necessários para plantio de sua
+        lavoura.
+      </p>
+    </Feature>
+    <Feature icon={<FaProjectDiagram />} title="Confiança e experiência">
+      <p>
+        Possuímos mais de 25 anos de experiência na prestação de serviços de
+        transportes, plantio e colheitas de lavouras próprias e de terceiros.
+      </p>
+    </Feature>
+  </Grid>
 );
