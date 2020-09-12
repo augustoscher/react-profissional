95e118b26e3caa95bc9365aeb0543f708b90e758
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-09-12 20:47:10 -0300

    Refact and create CardBody component

--- src/components/atoms/Card.js
+++ src/components/atoms/Card.js
@@ -2,15 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 // import PropTypes from 'prop-types';
 
-import Heading from '../atoms/Heading';
-import Button from '../atoms/Button';
-
 const Root = styled.div`
   background-color: #fff;
   border-radius: 4px;
 `;
 
-const Body = styled.div`
+const StyledBody = styled.div`
   padding: 16px;
 
   h6 {
@@ -18,20 +15,12 @@ const Body = styled.div`
   }
 `;
 
-const Card = () => (
+export const CardBody = ({ children }) => <StyledBody>{children}</StyledBody>;
+
+const Card = ({ children }) => (
   <Root>
     <div></div>
-    <Body>
-      <Heading>
-        <h6>Colheitas</h6>
-      </Heading>
-      <p>Nostrud nisi veniam do veniam ea sunt ipsum do.</p>
-      <div>
-        <Button variant="link" color="primary">
-          Saiba mais
-        </Button>
-      </div>
-    </Body>
+    {children}
   </Root>
 );
 
--- src/components/atoms/Card.stories.js
+++ src/components/atoms/Card.stories.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import Card from './Card';
+import Card, { CardBody } from './Card';
 import Section from '../molecules/Section';
+
+import Heading from '../atoms/Heading';
+import Button from '../atoms/Button';
 
 export default {
   title: 'Components/Atoms/Card',
@@ -9,6 +12,18 @@ export default {
 
 export const usage = () => (
   <Section inverse>
-    <Card></Card>
+    <Card>
+      <CardBody>
+        <Heading>
+          <h6>Colheitas</h6>
+        </Heading>
+        <p>Nostrud nisi veniam do veniam ea sunt ipsum do.</p>
+        <div>
+          <Button variant="link" color="primary">
+            Saiba mais
+          </Button>
+        </div>
+      </CardBody>
+    </Card>
   </Section>
 );
