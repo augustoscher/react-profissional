3c634d47b444814974cd7144d2a12ec64c246fc6
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-09-12 23:50:16 -0300

    Implementing ProductGrid component behavior

--- src/components/organisms/ProductGrid.js
+++ src/components/organisms/ProductGrid.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import Grid from '../atoms/Grid';
@@ -6,26 +7,45 @@ import Card, { CardBody, CardMedia } from '../atoms/Card';
 import Heading from '../atoms/Heading';
 import Button from '../atoms/Button';
 
-const ProductGrid = ({ products }) => (
-  <Grid md={3}>
-    {products.map(product => (
-      <Card key={product.id}>
-        <CardMedia image={product.image} />
-        <CardBody>
-          <Heading>
-            <h6>{product.title}</h6>
-          </Heading>
-          <p>{product.summary}</p>
-          <div>
-            <Button variant="linked" color="primary">
-              Saiba mais
-            </Button>
-          </div>
-        </CardBody>
-      </Card>
-    ))}
-  </Grid>
-);
+const Toolbar = styled.div`
+  margin-top: 40px;
+  text-align: center;
+`;
+
+const ProductGrid = ({ products }) => {
+  const [showAll, setShowAll] = useState(false);
+  const filteredProducts = showAll ? products : products.slice(0, 3);
+
+  return (
+    <>
+      <Grid md={3}>
+        {filteredProducts.map(product => (
+          <Card key={product.id}>
+            <CardMedia image={product.image} />
+            <CardBody>
+              <Heading>
+                <h6>{product.title}</h6>
+              </Heading>
+              <p>{product.summary}</p>
+              <div>
+                <Button variant="linked" color="primary">
+                  Saiba mais
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+        ))}
+      </Grid>
+      {!showAll && (
+        <Toolbar>
+          <Button variant="outlined" onClick={() => setShowAll(true)}>
+            Lista completa de serviços
+          </Button>
+        </Toolbar>
+      )}
+    </>
+  );
+};
 
 ProductGrid.defaultProps = {
   products: [],
